52e7cfcd3c835bce2e548817d9cd6a3e3c3ae4ed
Ricardo Merlin <rcrdmerlin@gmail.com>
2023-11-01 15:45:49 -0400

    Uploading objects lab

--- index.js
+++ index.js
@@ -1,1 +1,28 @@
-// Write your solution in this file!
+const employee = {
+    name: "Ricardo",
+    streetAddress: "Home"
+}
+
+function updateEmployeeWithKeyAndValue(employee, key, value) {
+    const newEmployee = { ...employee};
+    newEmployee[key] = value;
+    return newEmployee;
+}
+
+function destructivelyUpdateEmployeeWithKeyAndValue(employee, key, value) {
+    const desNewEmployee = employee;
+    desNewEmployee[key] = value;
+    return desNewEmployee;
+}
+
+function deleteFromEmployeeByKey(employee, key) {
+    const newerEmployee = { ...employee};
+    newerEmployee[key] = undefined
+    return newerEmployee;
+}
+
+function destructivelyDeleteFromEmployeeByKey(employee, key) {
+    const evenNewerEmployee = employee;
+    evenNewerEmployee[key] = undefined
+    return evenNewerEmployee;
+}
